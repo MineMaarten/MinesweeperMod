12c497e637a31c15041a03b394ac437bb183579a
Don <blodgett.donald@gmail.com>
2015-05-06 22:35:29 -0400

    Bring click handler in line with comments allowing a user in creative mode to use a sword to interact with minesweeper game. Also fixes bug which caused double rewards.

--- src/minesweeperMod/common/ForgeEventHandler.java
+++ src/minesweeperMod/common/ForgeEventHandler.java
@@ -1,5 +1,10 @@
 package minesweeperMod.common;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -13,13 +18,31 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler{
     /**
-     * Used to open a Minesweeper block in creative, when the player is holding a sword.
+     * Used to open a Minesweeper block in creative, when the player is holding a (vanilla) sword.
      * @param event
      */
     @SubscribeEvent
     public void onPlayerClick(PlayerInteractEvent event){
-        if(event.action == Action.LEFT_CLICK_BLOCK && event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z) == MinesweeperMod.blockMinesweeper) {
-            event.entityPlayer.worldObj.getBlock(event.x, event.y, event.z).onBlockClicked(event.entityPlayer.worldObj, event.x, event.y, event.z, event.entityPlayer);
+    	EntityPlayer player = event.entityPlayer;
+    	if (player.capabilities.isCreativeMode) {
+    		if (isSword(player.getCurrentEquippedItem())) {
+	    		Block block = player.worldObj.getBlock(event.x, event.y, event.z);
+	    		if(event.action == Action.LEFT_CLICK_BLOCK && block == MinesweeperMod.blockMinesweeper) {
+	        		block.onBlockClicked(player.worldObj, event.x, event.y, event.z, player);
+	        		event.setCanceled(true);
+	        	}
+    		}
         }
+    }
+    
+    private boolean isSword(ItemStack equipped) {
+    	if (equipped == null) {
+    		return false;
+    	}
+    	Item item = equipped.getItem();
+    	if (item == Items.diamond_shovel || item == Items.golden_sword || item == Items.iron_sword || item == Items.stone_sword || item == Items.wooden_sword) {
+    		return true;
+    	}
+    	return false;
     }
 }
